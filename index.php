0afb573d83a285149da01fe13fd0a78b1422522d
Quentin Brouillet <quent07@hotmail.com>
2020-04-28 10:31:02 +0200

    :sparkles: import DB ANIMAL

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+$bdd = new PDO('mysql:host=localhost;dbname=wf3zoo;charset=utf8;port=8889', 'root', 'root');
+$request = "SELECT * FROM ANIMAL";
+$response = $bdd->query($request);
+$animals = $response->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($animals)
+?>
+
 <!doctype html>
 <html lang="en">
 
